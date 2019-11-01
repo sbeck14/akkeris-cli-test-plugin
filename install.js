8bed7057b0bfa73237334483e4c3e6ed925111e6
Sam Beckett <sbeck14@me.com>
2019-11-01 13:39:24 -0600

    Update install.js

--- install.js
+++ install.js
@@ -1,3 +1,5 @@
 module.exports = function(cross_spawn) {
+  console.log('Installing test plugin...');
+  console.log(cross_spawn);
   cross_spawn.sync('npm', ['install'], {cwd:process.cwd(), env:process.env, stdio:'ignore'});
 };
