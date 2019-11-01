66b9fc52bf2544541f92cd333ec83aaab6950bdf
Sam Beckett <sbeck14@me.com>
2019-11-01 13:55:41 -0600

    removed output

--- install.js
+++ install.js
@@ -1,5 +1,3 @@
 module.exports = function(cross_spawn) {
-  console.log('\nInstalling test plugin...');
   cross_spawn.sync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
-  console.log('npm install finished!');
 };
