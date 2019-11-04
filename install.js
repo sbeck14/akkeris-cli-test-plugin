a1713cf64b31eb805331d77c1673640568dd4097
Sam Beckett <sbeck14@me.com>
2019-11-04 10:25:53 -0700

    new and old index.js

--- install.js
+++ install.js
@@ -1,3 +1,7 @@
 const proc = require('child_process');
 
 proc.spawnSync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
+
+module.exports = function(cross_spawn) {
+  cross_spawn.sync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
+};
