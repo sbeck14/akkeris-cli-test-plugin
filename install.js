f0fb2e2339b26843769d41b89cd0583ceb1f34f3
Sam Beckett <sbeck14@me.com>
2019-11-04 10:30:36 -0700

    Update install.js

--- install.js
+++ install.js
@@ -1,9 +1,8 @@
-const proc = require('child_process');
-
-// Old way of installing
-proc.spawnSync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
-
-// New way of installing
 module.exports = function(cross_spawn) {
-  cross_spawn.sync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
+  if (!cross_spawn) {
+    const proc = require('child_process');
+    proc.spawnSync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
+  } else {
+    cross_spawn.sync('npm', ['install'], { cwd: __dirname, env: process.env, stdio: 'ignore' });
+  }
 };
